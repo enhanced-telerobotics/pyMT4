878551d87650530655ff496e2bacf219274fc5f8
Shuyuan Yang <me@yangshuyuan.cn>
2025-07-15 13:05:23 -0400

    Add: initial package configuration and setup for pyMT4

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-VERSION = "0.1.1"
-DESCRIPTION = "Python binding for MicronTracker 4"
+package_name = 'pyMT4'
 
 setup(
-    name='pyMT4',
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    name=package_name,
+    version='0.1.2',
+    packages=find_packages(),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=[
+        'setuptools',
+        'numpy',
+        'scipy',
+    ],
+    zip_safe=True,
     author='Shuyuan Yang',
     author_email='sxy841@case.edu',
     url='https://github.com/enhanced-telerobotics/pyMT4',
     license='MIT',
-    packages=find_packages(),
-    install_requires=[
-        'numpy',
-    ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Microsoft :: Windows',
-    ],
-    python_requires='>=3.8',
+    description='Python binding for MicronTracker 4',
+    entry_points={
+        'console_scripts': [
+            'tf_publisher = pyMT4.tf_publisher:main',
+        ],
+    },
 )
